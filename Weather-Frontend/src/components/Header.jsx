c335acd86e23a245b423109aa7e6d7f887215455
Aida Fuentes <afdlf2306@gmail.com>
2025-07-13 19:12:57 +0200

    estructura header

---
+++ Weather-Frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+function Header() {
+  return (
+    <header className="py-6 text-4xl font-bold text-white bg-blue-600 shadow-md">
+      <h1>Mood Weather 🌦️</h1>
+    </header>
+  );
+}
+
+export default Header
