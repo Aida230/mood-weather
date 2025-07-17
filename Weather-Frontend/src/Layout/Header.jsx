059ba4642184ea6f4f3461a1d39923dc30e001bf
Aida Fuentes <afdlf2306@gmail.com>
2025-07-17 17:48:55 +0200

    primer stilo basico

--- Weather-Frontend/src/Layout/Header.jsx
+++ Weather-Frontend/src/Layout/Header.jsx
@@ -1,10 +1,13 @@
 function Header() {
   return (
-    <header className="bg-gradient-to-r from-blue-500 to-indigo-600 shadow-lg">
-      <div className="max-w-7xl mx-auto px-6 py-8 flex items-center justify-between">
-        <h1 className="text-3xl sm:text-4xl font-extrabold text-white tracking-tight">
-          Mood Weather <span className="ml-2">🌦️</span>
-        </h1>
+    <header
+      className="h-[200px] w-full bg-cover bg-center bg-no-repeat flex items-center justify-center text-white"
+      style={{ backgroundImage: "url('/clima-tipos.jpg')" }}
+    >
+      <div className="text-center bg-black/50 p-6 rounded">
+      
+        <h1 className="text-4xl font-bold">Mood Weather 🌤️</h1>
+        <p className="text-xl mt-2">Tu clima diario con una dosis de ánimo</p>
       </div>
     </header>
   );
--- Weather-Frontend/src/main.jsx
+++ Weather-Frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import Header from "./Layout/Header.jsx";
+import App from './App.jsx'
 import Footer from "./Layout/Footer.jsx"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
